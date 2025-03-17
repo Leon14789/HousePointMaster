c6d58c509f0c9edc2c1c4fcdc88c64bcdd5e424a
Leon14789 <leoortizinho@gmail.com>
2025-03-17 17:31:00 -0300

    Footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,8 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
-</div>
+<link rel="stylesheet" href="assets/css/footer.css">
+<body class="d-flex flex-column">
+  <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Direitos Reservados a Etec Professor Elias Miguel Junior &copy; Desenvolvidor por: <a href="https://www.linkedin.com/in/leonardo-alves-4691b2269/">Leonardo O. Alves</a> </small>
+    </div>
+  </footer>
+</body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,6 @@
 <x-header />
+
+
+<h1>AQUI VAI FICAR O RANKING</h1>
+
+<x-footer />
